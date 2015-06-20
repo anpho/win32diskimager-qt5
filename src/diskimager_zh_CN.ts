d318b942fe043c4ef1a6dbf326f83a2c4f68931a
Tobin Davis <gruemaster@gmail.com>
2015-06-20 00:18:05 -0700

    Added: Simplified Chinese (thanks Lee Chenhwa)

---
+++ src/diskimager_zh_CN.ts
@@ -0,0 +1,397 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="17"/>
+        <source>Win32 Disk Imager</source>
+        <translation>Win32 磁盘映像工具</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="36"/>
+        <source>Image File</source>
+        <translation>映像文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="51"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="65"/>
+        <source>Device</source>
+        <translation>设备</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="122"/>
+        <source>If checked, will generate the MD5 hash for the image file</source>
+        <translation>选中则为映像文件产生MD5校验值</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="141"/>
+        <source>MD5 Hash: </source>
+        <translation>MD5校验值: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="164"/>
+        <source>Progress</source>
+        <translation>任务进度</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="182"/>
+        <source>%p%</source>
+        <translation>%p%</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="216"/>
+        <source>Cancel current process.</source>
+        <translation>取消任务。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="219"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="235"/>
+        <source>Read data from &apos;Device&apos; to &apos;Image File&apos;</source>
+        <translation>从“设备”向“映像文件”传送数据</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="238"/>
+        <source>Read</source>
+        <translation>读取</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="251"/>
+        <source>Write data from &apos;Image File&apos; to &apos;Device&apos;</source>
+        <translation>从“映像文件”向“设备”传送数据</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="254"/>
+        <source>Write</source>
+        <translation>写入</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="267"/>
+        <source>Exit Win32 Disk Imager</source>
+        <translation>退出 Win32 Disk Imager</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="270"/>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="101"/>
+        <location filename="mainwindow.cpp" line="111"/>
+        <source>Exit?</source>
+        <translation>退出?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="101"/>
+        <source>Exiting now will result in a corrupt image file.
+Are you sure you want to exit?</source>
+        <translation>现在退出映像文件将不完整.
+仍然退出?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="111"/>
+        <source>Exiting now will result in a corrupt disk.
+Are you sure you want to exit?</source>
+        <translation>现在退出将造成磁盘文件不完整.
+仍然退出?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="123"/>
+        <source>Select a disk image</source>
+        <translation>选择一个磁盘映像</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="148"/>
+        <source>Generating...</source>
+        <translation>生成中...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="194"/>
+        <source>Cancel?</source>
+        <translation>取消?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="194"/>
+        <source>Canceling now will result in a corrupt destination.
+Are you sure you want to cancel?</source>
+        <translation>现在取消会造成目的文件不完整.
+仍然取消?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="244"/>
+        <location filename="mainwindow.cpp" line="324"/>
+        <location filename="mainwindow.cpp" line="444"/>
+        <location filename="mainwindow.cpp" line="526"/>
+        <source>Write Error</source>
+        <translation>写入出错</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="244"/>
+        <location filename="mainwindow.cpp" line="444"/>
+        <source>Image file cannot be located on the target device.</source>
+        <translation>在目标设备上找不到映像文件.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="254"/>
+        <source>Confirm overwrite</source>
+        <translation>确认覆盖</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="254"/>
+        <source>Writing to a physical device can corrupt the device.
+(Target Device: %1 &quot;%2&quot;)
+Are you sure you want to continue?</source>
+        <translation>写入物理设备可能会造成该设备不可使用.
+(设备: %1 &quot;%2&quot;)
+是否继续?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="325"/>
+        <source>Not enough space on disk: Size: %1 sectors  Available: %2 sectors  Sector size: %3</source>
+        <translation>该磁盘空间不够: 容量: %1 扇区  可以使用: %2 扇区  扇区大小: %3</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="404"/>
+        <location filename="mainwindow.cpp" line="409"/>
+        <location filename="mainwindow.cpp" line="414"/>
+        <location filename="mainwindow.cpp" line="428"/>
+        <source>File Error</source>
+        <translation>文件错误</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="404"/>
+        <source>The selected file does not exist.</source>
+        <translation>文件不存在.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="409"/>
+        <source>You do not have permision to read the selected file.</source>
+        <translation>没有读取该文件的权限.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="414"/>
+        <source>The specified file contains no data.</source>
+        <translation>该文件为空文件.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="418"/>
+        <source>Done.</source>
+        <translation>完成.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="422"/>
+        <location filename="mainwindow.cpp" line="609"/>
+        <source>Complete</source>
+        <translation>完成</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="422"/>
+        <source>Write Successful.</source>
+        <translation>写入成功.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="428"/>
+        <source>Please specify an image file to use.</source>
+        <translation>请选中要使用的映像文件.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="449"/>
+        <source>Confirm Overwrite</source>
+        <translation>确认覆盖</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="449"/>
+        <source>Are you sure you want to overwrite the specified file?</source>
+        <translation>缺认覆盖已存在的文件?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="526"/>
+        <source>Disk is not large enough for the specified image.</source>
+        <translation>硬盘空间不够.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="609"/>
+        <source>Read Successful.</source>
+        <translation>读取成功.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="613"/>
+        <source>File Info</source>
+        <translation>文件信息</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="613"/>
+        <source>Please specify a file to save data to.</source>
+        <translation>请选择要保存的文件路径.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="disk.cpp" line="51"/>
+        <location filename="disk.cpp" line="236"/>
+        <location filename="disk.cpp" line="325"/>
+        <location filename="disk.cpp" line="340"/>
+        <source>File Error</source>
+        <translation>文件错误</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="51"/>
+        <source>An error occurred when attempting to get a handle on the file.
+Error %1: %2</source>
+        <translation>获取文件句柄失败.
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="70"/>
+        <location filename="disk.cpp" line="213"/>
+        <source>Device Error</source>
+        <translation>设备错误</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="71"/>
+        <source>An error occurred when attempting to get a handle on the device.
+Error %1: %2</source>
+        <translation>获取设备句柄失败.
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="89"/>
+        <location filename="disk.cpp" line="417"/>
+        <source>Volume Error</source>
+        <translation>卷错误</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="90"/>
+        <source>An error occurred when attempting to get a handle on the volume.
+Error %1: %2</source>
+        <translation>获取卷句柄失败
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="107"/>
+        <source>Lock Error</source>
+        <translation>锁错误</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="108"/>
+        <source>An error occurred when attempting to lock the volume.
+Error %1: %2</source>
+        <translation>锁该卷时发生错误.
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="125"/>
+        <source>Unlock Error</source>
+        <translation>解锁失败</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="126"/>
+        <source>An error occurred when attempting to unlock the volume.
+Error %1: %2</source>
+        <translation>解锁改卷时失败.
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="143"/>
+        <source>Dismount Error</source>
+        <translation>卸载失败 </translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="144"/>
+        <source>An error occurred when attempting to dismount the volume.
+Error %1: %2</source>
+        <translation>卸载卷时发生错误	.
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="171"/>
+        <source>Read Error</source>
+        <translation>读取失败</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="172"/>
+        <source>An error occurred when attempting to read data from handle.
+Error %1: %2</source>
+        <translation>从该句柄中读取数据发生错误.
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="194"/>
+        <source>Write Error</source>
+        <translation>写入失败</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="195"/>
+        <source>An error occurred when attempting to write data to handle.
+Error %1: %2</source>
+        <translation>向该句柄写入数据时发生错误.
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="214"/>
+        <source>An error occurred when attempting to get the device&apos;s geometry.
+Error %1: %2</source>
+        <translation>无法获取设备容量.
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="237"/>
+        <source>An error occurred while getting the file size.
+Error %1: %2</source>
+        <translation>无法获取文件大小.
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="259"/>
+        <source>Free Space Error</source>
+        <translation>剩余空间错误</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="260"/>
+        <source>Failed to get the free space on drive %1.
+Error %2: %3
+Checking of free space will be skipped.</source>
+        <translation>无法得到驱动器剩余空间 %1.
+错误 %2: %3
+已忽略剩余空间检查.</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="326"/>
+        <source>An error occurred while getting the device number.
+This usually means something is currently accessing the device;please close all applications and try again.
+
+Error %1: %2</source>
+        <translation>获取设备号失败.
+通常发生该错误是由于其他程序正在访问该设备;请关闭相关程序重试.
+
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="341"/>
+        <source>An error occurred while querying the properties.
+This usually means something is currently accessing the device; please close all applications and try again.
+
+Error %1: %2</source>
+        <translation>获取属性信息失败.
+通常发生该错误是由于其他程序正在访问该设备;请关闭相关程序重试.
+
+错误 %1: %2</translation>
+    </message>
+    <message>
+        <location filename="disk.cpp" line="418"/>
+        <source>An error occurred when attempting to get a handle on %3.
+Error %1: %2</source>
+        <translation>获取句柄时发生错误 %3.
+错误 %1: %2</translation>
+    </message>
+</context>
+</TS>
